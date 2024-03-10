bb2dc441ea0175ff8d36b02135e820bac6e08274
s3xysteak <j.yulia@qq.com>
2024-03-10 12:19:54 +0800

    feat: Use readFileSync instead of async.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { readFile, writeFileSync } from 'node:fs'
+import { readFileSync, writeFileSync } from 'node:fs'
 import { join, normalize } from 'node:path'
 
 import { jsonParser } from '@core/jsonParser'
@@ -9,14 +9,10 @@ function tauriVersion(options: VersionOption, _path = './src-tauri/') {
   const path = normalize(join(process.cwd(), _path))
 
   /** Cargo */
-  readFile(join(path, 'Cargo.toml'), 'utf-8', (err, data) => {
-    if (err) throw err
-    writeFileSync(join(path, 'Cargo.toml'), tomlParser(data, options))
-  })
+  const tomlContent = readFileSync(join(path, 'Cargo.toml'), 'utf-8')
+  writeFileSync(join(path, 'Cargo.toml'), tomlParser(tomlContent, options))
   /** Conf */
-  readFile(join(path, 'tauri.conf.json'), 'utf-8', (err, data) => {
-    if (err) throw err
-    writeFileSync(join(path, 'tauri.conf.json'), jsonParser(data, options))
-  })
+  const confContent = readFileSync(join(path, 'tauri.conf.json'), 'utf-8')
+  writeFileSync(join(path, 'tauri.conf.json'), jsonParser(confContent, options))
 }
 export default tauriVersion
