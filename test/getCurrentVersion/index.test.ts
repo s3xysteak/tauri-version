035e5a3243d95f3234db11fdd8b5bc344218828e
Philipp Wirtenberger <philipp.wirtenberger@actus-tax.com>
2024-11-14 14:07:02 +0100

    test: Add test for version mismatch.

--- test/getCurrentVersion/index.test.ts
+++ test/getCurrentVersion/index.test.ts
@@ -5,4 +5,8 @@ describe('getCurrentVersion', () => {
     it('should return version when all versions match', () => {
         expect(getCurrentVersion('test/__e2e__')).toBe('0.0.1')
     })
+
+    it('should throw error when versions mismatch', () => {
+        expect(() => getCurrentVersion('test/getCurrentVersion/files')).toThrow('Version mismatch')
+    })
 })
